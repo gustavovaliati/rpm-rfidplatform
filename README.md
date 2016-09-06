efc264111e39b50026138f5ff7ce5047199d055e
Gustavo Valiati <gustavovaliati@gmail.com>
2016-09-06 15:47:41 -0300

    First install uninstall - clean

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rpm-rfidplatform
+
+## Building the RPM package
+
+1. To build the package execute the 'build.sh' script.
+
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo ls;
+sudo ls > /dev/null;
 /usr/src/redhat/SOURCES/prepare-source.sh &&
 sudo rpmbuild -ba /usr/src/redhat/SPECS/rfidplatform.spec;
